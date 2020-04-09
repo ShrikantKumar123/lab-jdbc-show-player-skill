afbdf85eacd13d36f83a590bb025560bf36fbdfa
unknown <shrikant2994anil@gmail.com>
2020-04-09 22:28:59 +0530

    FSJ0320A005

--- src/dao/SkillDAO.java
+++ src/dao/SkillDAO.java
@@ -1,1 +1,48 @@
+package dao;
+
+import java.sql.Connection;
+
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import model.Skill;
+import utility.ConnectionManager;
+
+public class SkillDAO
+
+//METHOD TO HANDLE ALL DATABASE RELATED OPERATIONS
+{
+	static List<Skill> list=new ArrayList<Skill>();
+
+	public List< Skill > listAllSkills() throws Exception
+	{
+		Connection con = ConnectionManager.getConnection();
+
+		Statement statement=  con.createStatement();
+
+		ResultSet resultset =statement.executeQuery("select * from SKILL ");
+		
+		while(resultset.next())
+		{
+
+			long id=resultset.getLong(1);
+			String name=resultset.getString(2);
+			Skill skill=new Skill(id,name);
+			
+			list.add(skill);
+
+		}
+
+		return list;
+
+
+	}
+
+
+
+
+}
+
 
--- src/utility/ConnectionManager.java
+++ src/utility/ConnectionManager.java
@@ -1,1 +1,47 @@
 
+package utility;
+
+import java.io.InputStream;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.Properties;
+
+public class ConnectionManager {
+	
+	public static Connection getConnection() throws Exception 
+	{
+		
+		
+Properties prop=loadPropertiesFile();
+{
+		
+		String driver = prop.getProperty("driver");
+		String url= prop.getProperty("url");
+		String user=prop.getProperty("username");
+		String password=prop.getProperty("password");
+		
+		//REGISTER THE DRIVER
+		 Class.forName(driver);
+		 
+		 //CONNECTION OBJECT
+		 Connection con;
+		 con=DriverManager.getConnection(url,user,password);
+		 
+		 return con;
+		 
+
+	}
+	}
+		
+public static Properties loadPropertiesFile() throws Exception
+{
+		Properties prop = new Properties();
+		InputStream in = ConnectionManager.class.getClassLoader().getResourceAsStream("jdbc.properties");
+		prop.load(in);
+		in.close();
+		return prop;
+	
+} 
+	
+}
